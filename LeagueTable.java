80bad9e33d10c9a1df2d1ceda2136732de6fc87d
wangzihe2008-afk <wangzihe2008@gmail.com>
2026-09-16 22:32:00 -0400

    Add LeagueTable.java

---
+++ LeagueTable.java
@@ -0,0 +1,96 @@
+public class LeagueTable {
+
+    private Team[] teams;
+
+    public LeagueTable(Team[] teams) {
+        this.teams = teams;
+    }
+
+    public Team[] getStandings() {
+        Team[] standings = new Team[teams.length];
+
+        for (int i = 0; i < teams.length; i++) {
+            standings[i] = teams[i];
+        }
+
+        for (int i = 0; i < standings.length - 1; i++) {
+            for (int j = 0; j < standings.length - 1 - i; j++) {
+                if (shouldSwap(standings[j], standings[j + 1])) {
+                    Team temp = standings[j];
+                    standings[j] = standings[j + 1];
+                    standings[j + 1] = temp;
+                }
+            }
+        }
+
+        return standings;
+    }
+
+    private boolean shouldSwap(Team first, Team second) {
+        if (first.getPoints() != second.getPoints()) {
+            return first.getPoints() < second.getPoints();
+        }
+
+        if (first.getGoalDifference() != second.getGoalDifference()) {
+            return first.getGoalDifference() < second.getGoalDifference();
+        }
+
+        if (first.getGoalsFor() != second.getGoalsFor()) {
+            return first.getGoalsFor() < second.getGoalsFor();
+        }
+
+        if (first.getWins() != second.getWins()) {
+            return first.getWins() < second.getWins();
+        }
+
+        return false;
+    }
+
+    public int getTeamPosition(Team team) {
+        if (team == null) {
+            return -1;
+        }
+
+        Team[] standings = getStandings();
+
+        for (int i = 0; i < standings.length; i++) {
+            if (standings[i] == team) {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public Team getLeader() {
+        Team[] standings = getStandings();
+        return standings.length == 0 ? null : standings[0];
+    }
+
+    public String getTableText() {
+        Team[] standings = getStandings();
+
+        String text = String.format("%-4s %-24s %3s %3s %3s %3s %4s %4s %4s %4s%n",
+                "POS", "TEAM", "P", "W", "D", "L", "GF", "GA", "GD", "PTS");
+        text += "----------------------------------------------------------------------------\n";
+
+        for (int i = 0; i < standings.length; i++) {
+            Team team = standings[i];
+            int played = team.getWins() + team.getDraws() + team.getLosses();
+
+            text += String.format("%-4d %-24s %3d %3d %3d %3d %4d %4d %4d %4d%n",
+                    i + 1,
+                    team.getName(),
+                    played,
+                    team.getWins(),
+                    team.getDraws(),
+                    team.getLosses(),
+                    team.getGoalsFor(),
+                    team.getGoalsAgainst(),
+                    team.getGoalDifference(),
+                    team.getPoints());
+        }
+
+        return text;
+    }
+}
